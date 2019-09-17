73c27892704e3300b2b99cdf290f02b2a98c78d4
sepremento <sepremento@gmail.com>
2019-09-17 14:13:38 +0500

    Creating functions
    Created makeCacheMatrix() and cacheSolve(), which seem to work correctly.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is an overall similar to the example makeVector() and cachemean() functions
+## I managed to get inverted matrices.
 
-## Write a short comment describing this function
+## This function creates an object with four functions and two variables to store values.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mx_inv <- NULL
+  set <- function(y){
+    x <<- y
+    mx_inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) mx_inv <<- solve
+  getinv <- function() mx_inv
+  list(set = set, get = get,setinv = setinv,getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function makes the inverted matrix if it is new and gets cached matrix if it's not new.
 
 cacheSolve <- function(x, ...) {
+  mx_inv <- x$getinv()
+  if(!is.null(mx_inv)) {
+    message("getting cached inverse matrix")
+    return(mx_inv)
+  }
+  data <- x$get()
+  mx_inv <- solve(data)
+  x$setinv(mx_inv)
+  mx_inv
         ## Return a matrix that is the inverse of 'x'
 }
